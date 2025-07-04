b38557cbca131b8932a7c17572ebcc46523d4b70
abdullah-alfahim <abdullahsvoice@gmail.com>
2025-07-05 02:17:31 +0600

    new file:   blog/accounts/__pycache__/__init__.cpython-312.pyc 	new file:   blog/accounts/__pycache__/admin.cpython-312.pyc 	new file:   blog/accounts/__pycache__/apps.cpython-312.pyc 	new file:   blog/accounts/__pycache__/models.cpython-312.pyc 	new file:   blog/accounts/__pycache__/urls.cpython-312.pyc 	new file:   blog/accounts/__pycache__/views.cpython-312.pyc 	new file:   blog/accounts/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   blog/accounts/views.py 	modified:   blog/blog/__pycache__/settings.cpython-312.pyc 	modified:   blog/blog/__pycache__/urls.cpython-312.pyc 	modified:   blog/blog/settings.py 	modified:   blog/blog/urls.py 	modified:   blog/config/__pycache__/views.cpython-312.pyc 	new file:   blog/config/templates/config/blog_create.html 	modified:   blog/config/templates/config/blog_list.html 	modified:   blog/config/views.py 	modified:   blog/db.sqlite3 	new file:   blog/media/250507170550644.jpg 	new file:   blog/media/250611185004332.jpg 	modified:   blog/templates/registration/login.html 	modified:   blog/templates/registration/signup.html

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
@@ -9,9 +8,6 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/signup.html'
-    success_url = reverse_lazy('login')  # Redirect to login page after successful signup
+    success_url = reverse_lazy("login")  # Redirect to login page after successful signup
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Sign Up'
-        return context
+    
--- blog/config/views.py
+++ blog/config/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Blog
 
 # Create your views here.
@@ -9,4 +9,28 @@ class HomeView(ListView):
     model = Blog
     context_object_name = 'blogs'
 
+
+
+
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin # Import LoginRequiredMixin
+from .models import Blog # Assuming Blog model is in config/models.py
+
+class BlogCreateView(LoginRequiredMixin, CreateView): # Add LoginRequiredMixin here
+    model = Blog
+    success_url = reverse_lazy('home')  # Use reverse_lazy for better practice, assuming 'home' is your home page URL name
+    fields = ['heading', 'keywords', 'image', 'description']  # Specify the fields you want in the form
+    template_name = 'config/blog_create.html'
+
+    def form_valid(self, form):
+        
+        form.instance.owner = self.request.user
+        
+        # Call the parent class's form_valid method to save the object
+        # This will save the form.instance (now with owner assigned) to the database
+        return super().form_valid(form)
     
+
+
+
