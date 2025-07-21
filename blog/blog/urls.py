0678ebd099849a3d37c67eb9dc2124e99d864f9a
abdullah-alfahim <abdullahsvoice@gmail.com>
2025-07-22 03:45:28 +0600

    b

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,22 +1,18 @@
-# project/urls.py
 from django.contrib import admin
-from django.urls import path, include # Make sure 'include' is imported
-from config.views import HomeView, BlogCreateView # Assuming config.views is where HomeView is
+from django.urls import path, include
+from config.views import HomeView, BlogCreateView, ProfileView
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.contrib.auth import views as auth_views # No longer explicitly needed here if using include('django.contrib.auth.urls')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-    path('blog/', BlogCreateView.as_view(), name='create'),  # Assuming you want to create a blog post
-    # Include the URLs for your custom accounts app
-    path("accounts/", include('accounts.urls')),  # Your custom accounts app URLs
-    path('accounts/', include('django.contrib.auth.urls')), # Django's built-in auth URLs (includes 'login', 'logout', 'password_change', etc.)
-    # REMOVE THIS LINE: path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('blog/', BlogCreateView.as_view(), name='create'),  
+    path("accounts/", include('accounts.urls')),  
+    path('accounts/', include('django.contrib.auth.urls')), 
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
 
-# BOTH static AND media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Removed trailing comma here, it was creating a tuple
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/config/views.py
+++ blog/config/views.py
@@ -2,35 +2,29 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import Blog
 
-# Create your views here.
 
+
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Blog
 
 class HomeView(ListView):
     model = Blog
     context_object_name = 'blogs'
 
 
-
-
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin # Import LoginRequiredMixin
-from .models import Blog # Assuming Blog model is in config/models.py
-
-class BlogCreateView(LoginRequiredMixin, CreateView): # Add LoginRequiredMixin here
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Blog
-    success_url = reverse_lazy('home')  # Use reverse_lazy for better practice, assuming 'home' is your home page URL name
-    fields = ['heading', 'keywords', 'image', 'description']  # Specify the fields you want in the form
+    success_url = reverse_lazy('home')
+    fields = ['heading', 'keywords', 'image', 'description']
     template_name = 'config/blog_create.html'
 
     def form_valid(self, form):
-        
         form.instance.owner = self.request.user
-        
-        # Call the parent class's form_valid method to save the object
-        # This will save the form.instance (now with owner assigned) to the database
         return super().form_valid(form)
     
-
-
-
+class ProfileView(LoginRequiredMixin, ListView):
+    model = Blog
+    context_object_name = 'blogs'
+    template_name = 'config/profile.html'
