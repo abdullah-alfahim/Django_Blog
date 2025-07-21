c94876b76627c525ef1fbed773f0b2e4b917b64f
abdullah-alfahim <abdullahsvoice@gmail.com>
2025-07-22 04:40:51 +0600

    Fixed the unnoticed Profile section. I may change to Edit section in future

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from config.views import HomeView, BlogCreateView, ProfileView
+from config.views import HomeView, BlogCreateView, ProfileView, DeleteView, UpdateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,6 +11,8 @@ urlpatterns = [
     path("accounts/", include('accounts.urls')),  
     path('accounts/', include('django.contrib.auth.urls')), 
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('delete/<int:pk>/', DeleteView.as_view(), name='delete'),
+    path('update/<int:pk>/', UpdateView.as_view(), name='update'),
 ]
 
 if settings.DEBUG:
--- blog/config/views.py
+++ blog/config/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from .models import Blog
-
-
-
-from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Blog
@@ -28,3 +23,16 @@ class ProfileView(LoginRequiredMixin, ListView):
     model = Blog
     context_object_name = 'blogs'
     template_name = 'config/profile.html'
+
+class UpdateView(UpdateView):
+    model = Blog
+    success_url = reverse_lazy('profile')
+    fields = ['heading', 'keywords', 'image', 'description']
+    template_name = 'config/blog_create.html'
+
+
+class DeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('profile')
+    template_name = 'config/blog_delete.html'
+
